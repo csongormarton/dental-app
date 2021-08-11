0fa1cba7c634596e99414adcdd448272084e221a
Csongor Marton <martonjcs@gmail.com>
2021-08-11 19:08:33 +0300

    Added app url to navbar's app name

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="bg-gray-200 py-2 md:py-4">
     <div class="container px-4 mx-auto md:flex md:items-center">
         <div class="flex justify-between items-center">
-            <a href="#" class="font-bold text-xl text-gray-800">
+            <a href="{{ config('app.url') }}" class="font-bold text-xl text-gray-800">
                 @lang('template.app_name')
             </a>
         </div>
